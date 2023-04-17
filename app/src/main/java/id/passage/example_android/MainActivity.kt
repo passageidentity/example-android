822f3e9f71ed00417c2505cbd97c684819cae393
Ricky Padilla <rickycpadilla@gmail.com>
2023-04-17 16:45:02 -0600

    Importing Passage Android SDK library locally.

--- app/src/main/java/id/passage/example_android/MainActivity.kt
+++ app/src/main/java/id/passage/example_android/MainActivity.kt
@@ -2,10 +2,25 @@ package id.passage.example_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import id.passage.android.PassageAuth
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testRegister()
+    }
+
+    private fun testRegister() {
+        runBlocking {
+            val response = PassageAuth.register("ricky.padilla+1@passage.id")
+            Log.d(TAG, response ?: "No response")
+        }
     }
 }
