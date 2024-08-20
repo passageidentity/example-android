5ac5f0e5e29d13ba138567591816205ab6e571af
Sina <65910646+SinaSeylani@users.noreply.github.com>
2024-08-20 16:00:25 -0600

    Example app updated

--- app/src/main/java/id/passage/example_android/MainActivity.kt
+++ app/src/main/java/id/passage/example_android/MainActivity.kt
@@ -1,37 +1,63 @@
 package id.passage.example_android
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentContainerView
+import id.passage.android.Passage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainActivity: AppCompatActivity() {
-
+class MainActivity : AppCompatActivity() {
     private lateinit var fragmentContainerView: FragmentContainerView
+    private lateinit var passage: Passage
+    private val ioScope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fragmentContainerView = findViewById(R.id.nav_host_fragment_container)
+        passage = Passage(this, "CFKsqjyERaYC3DLqEexaJzKW")
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
+        // Handling Magic link redirect url
+        val magicLink = intent?.data?.getQueryParameter("psg_magic_link")
         val navHostFragment = fragmentContainerView.getFragment<Fragment>()
         val currentFragment = navHostFragment.childFragmentManager.fragments.first() ?: return
-        val magicLinkFragment = currentFragment as? MagicLinkFragment ?: return
-        val magicLink = intent?.data?.getQueryParameter("psg_magic_link") ?: return
-        magicLinkFragment.handleDeepLinkMagicLink(magicLink)
+        if (magicLink != null) {
+            val magicLinkFragment = currentFragment as? MagicLinkFragment ?: return
+            magicLinkFragment.handleDeepLinkMagicLink(magicLink)
+        }
+        // Handling Hosted Login redirect url
+        val code = intent?.data?.getQueryParameter("code")
+        val state = intent?.data?.getQueryParameter("state")
+        if (code != null && state != null) {
+            try {
+                ioScope.launch {
+                    passage.hosted.hostedAuthFinish(code, state)
+                    val loginFragment = currentFragment as? LoginFragment ?: return@launch
+                    loginFragment.handleDeepLink()
+                }
+            } catch (e: Exception) {
+                // Report the error
+            }
+        }
     }
-
 }
 
-fun FragmentActivity.showAlert(title: String, message: String) {
+fun FragmentActivity.showAlert(
+    title: String,
+    message: String,
+) {
     val dialogBuilder = AlertDialog.Builder(this)
-    dialogBuilder.setMessage(message)
+    dialogBuilder
+        .setMessage(message)
         .setCancelable(true)
         .setNegativeButton("Okay") { dialog, _ ->
             dialog.cancel()
